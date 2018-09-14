ecc14e9bc502ff7b23c358eeabad9ac8b758a62e
Omar A <oazzat19@gmail.com>
2018-09-14 05:13:19 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,3 @@
 def display_rainbow (colors)
-  puts "R: #{color[0]},"
-  puts "O: #{color[1]},"
-  puts "Y: #{color[2]},"
-  puts "G: #{color[3]},"
-  puts "B: #{color[4]},"
-  puts "I: #{color[5]},"
-  puts "V: #{color[6]},"
+  puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]},"
 end
